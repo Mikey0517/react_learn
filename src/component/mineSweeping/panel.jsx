bc417e9b7f1536c2c40270f39caf11308d381a11
兔子 <xuyanghaoyue@bonc.com.cn>
2019-08-02 18:18:58 +0800

    完善功能

--- src/component/mineSweeping/panel.jsx
+++ src/component/mineSweeping/panel.jsx
@@ -13,13 +13,20 @@ class MineItem extends Component {
     const { onClick, onContextMenu, status } = this.props;
     let className = 'col';
     let html = '';
-    if ( status === -1 ) {
-      className += ' mine';
-    } else {
-      if ( status === -2 ) {
-        html = '🚩';
-      } else if ( status > 0 ) {
-        html = status;
+    if ( typeof status !== 'undefined' ) {
+      if ( status > -2 ) {
+        className += ' open';
+        if ( status === -1 ) {
+          className += ' mine';
+        } else {
+          html = status === 0 ? '' : status;
+        }
+      } else {
+        if ( status === -2 ) {
+          html = '🚩';
+        } else {
+          html = '?';
+        }
       }
     }
     return (
@@ -42,13 +49,14 @@ class Panel extends Component {
     };
     this.type = {
       primary: {
-        x: 9,
-        y: 9,
+        xLength: 9,
+        yLength: 9,
         mine: 10
       }
     };
     this.mine = null;
     this.isClick = true;
+    this.openGrid = 0;
   }
 
   componentWillMount () {
@@ -69,41 +77,113 @@ class Panel extends Component {
 
   random () {
     const { type } = this.state;
-    const { x, y } = this.type[ type ];
-    let i = Math.floor( Math.random() * x );
-    let j = Math.floor( Math.random() * y );
+    const { xLength, yLength } = this.type[ type ];
+    let i = Math.floor( Math.random() * xLength );
+    let j = Math.floor( Math.random() * yLength );
     return i * 10 + j;
   }
 
   handleClick ( x, y, index ) {
-    const { cache } = this.state;
+    const { cache, type } = this.state;
     if ( cache.has( index ) || !this.isClick ) return false;
-    if ( this.mine.has( index ) ) {
-      this.isClick = true;
+    if ( this.searchGrid( index ) ) {
+      this.isClick = false;
       cache.set( index, -1 );
       this.setState( { cache } );
+      alert( '输了' )
     } else {
-
+      this.openGrid++;
+      let roundMine = 0;
+      let roundGridList = this.findRoundGrid( x, y );
+      let keyList = Object.keys( roundGridList );
+      keyList.map( ( key ) => {
+        let round = roundGridList[ key ];
+        if ( this.searchGrid( round[ 0 ] * 10 + round[ 1 ] ) ) roundMine++;
+      } );
+      cache.set( index, roundMine );
+      this.setState( { cache }, () => {
+        const { xLength, yLength, mine } = this.type[ type ];
+        if ( this.openGrid === xLength * yLength - mine ) {
+          alert( '赢了' )
+        } else {
+          if ( roundMine === 0 ) {
+            keyList.map( ( key ) => {
+              let round = roundGridList[ key ];
+              this.handleClick( round[ 0 ], round[ 1 ], round[ 0 ] * 10 + round[ 1 ] );
+            } );
+          }
+        }
+      } );
     }
   }
 
+  /**
+   * 查询格子是否有雷
+   * */
+  searchGrid ( index ) {
+    return this.mine.has( index );
+  }
+
+  findRoundGrid ( x, y ) {
+    const { type } = this.state;
+    const { xLength, yLength } = this.type[ type ];
+    let roundGridList = {
+      'top': [ x, y - 1 ],
+      'top-right': [ x + 1, y - 1 ],
+      'right': [ x + 1, y ],
+      'bottom-right': [ x + 1, y + 1 ],
+      'bottom': [ x, y + 1 ],
+      'bottom-left': [ x - 1, y + 1 ],
+      'left': [ x - 1, y ],
+      'top-left': [ x - 1, y - 1 ]
+    };
+    let keyList = Object.keys( roundGridList );
+    let dealData = ( position ) => {
+      for ( let i = 0; i < keyList.length; i++ ) {
+        let key = keyList[ i ];
+        if ( key.includes( position ) ) {
+          delete roundGridList[ key ];
+          keyList.splice( i, 1 );
+          i--;
+        }
+      }
+    };
+    if ( x === 0 ) dealData( 'left' );
+    if ( y === 0 ) dealData( 'top' );
+    if ( x === xLength - 1 ) dealData( 'right' );
+    if ( y === yLength - 1 ) dealData( 'bottom' );
+    return roundGridList;
+  }
+
   handleContextMenu ( index, e ) {
-    const { cache } = this.state;
-    if ( !cache.has( index ) ) cache.set( index, -2 );
-    this.setState( { cache } );
     e.preventDefault();
+    if ( !this.isClick ) return false;
+    const { cache } = this.state;
+    if ( !cache.has( index ) ) {
+      cache.set( index, -2 );
+    } else {
+      let code = cache.get( index );
+      if ( code === -2 ) {
+        cache.set( index, -3 );
+      } else if ( code === -3 ) {
+        cache.delete( index );
+      } else {
+        return false;
+      }
+    }
+    this.setState( { cache } );
   }
 
   render () {
     const { type, cache } = this.state;
-    const { x, y } = this.type[ type ];
+    const { xLength, yLength } = this.type[ type ];
     return (
       <div className='mine-sweeping-panel'>
         {
-          ( new Array( y ) ).fill( 0 ).map( ( item, j ) => (
+          ( new Array( yLength ) ).fill( 0 ).map( ( item, j ) => (
             <div className='row' key={ j }>
               {
-                ( new Array( x ) ).fill( 0 ).map( ( item, i ) => {
+                ( new Array( xLength ) ).fill( 0 ).map( ( item, i ) => {
                   let index = i * 10 + j;
                   return (
                     <MineItem
